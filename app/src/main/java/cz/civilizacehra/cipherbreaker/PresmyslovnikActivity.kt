ff2d4a3f941c93c4b212a026c89c862f77b5e334
Adam Sikora <adam.sikora73@gmail.com>
2019-09-28 13:56:07 +0200

    Make presmyslovnik UI components lazy

--- app/src/main/java/cz/civilizacehra/cipherbreaker/PresmyslovnikActivity.kt
+++ app/src/main/java/cz/civilizacehra/cipherbreaker/PresmyslovnikActivity.kt
@@ -1,7 +1,6 @@
 package cz.civilizacehra.cipherbreaker
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import android.os.Bundle
 import android.text.InputType
@@ -16,16 +15,16 @@ import java.util.regex.PatternSyntaxException
 
 class PresmyslovnikActivity : LocationActivity() {
 
-    private var sharedPreferences: SharedPreferences? = null
+    private val sharedPreferences by lazy {PreferenceManager.getDefaultSharedPreferences(this)}
 
-    private var goBtn: Button? = null
-    private var inputBox: EditText? = null
-    private var minLengthBox: EditText? = null
-    private var maxLengthBox: EditText? = null
-    internal var results: TextView? = null
-    private var modeSpinner: Spinner? = null
-    private var dictionarySpinner: Spinner? = null
-    private var svjz: CheckBox? = null
+    private val goBtn by lazy {findViewById<Button>(R.id.GoBtn)}
+    private val inputBox by lazy {findViewById<EditText>(R.id.inputEditText)}
+    private val minLengthBox by lazy {findViewById<EditText>(R.id.minEditText)}
+    private val maxLengthBox by lazy {findViewById<EditText>(R.id.maxEditText)}
+    internal val results by lazy {findViewById<TextView>(R.id.resultTextView)}
+    private val modeSpinner by lazy {findViewById<Spinner>(R.id.modeSpinner)}
+    private val dictionarySpinner by lazy {findViewById<Spinner>(R.id.dictionarySpinner)}
+    private val svjz by lazy {findViewById<CheckBox>(R.id.svjzCheckBox)}
 
     private var enDict: Dictionary? = null
     private var czPJDict: Dictionary? = null
@@ -39,16 +38,7 @@ class PresmyslovnikActivity : LocationActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_presmyslovnik)
 
-        goBtn = findViewById(R.id.GoBtn)
-        inputBox = findViewById(R.id.inputEditText)
-        minLengthBox = findViewById(R.id.minEditText)
-        maxLengthBox = findViewById(R.id.maxEditText)
-        results = findViewById(R.id.resultTextView)
-        modeSpinner = findViewById(R.id.modeSpinner)
-        dictionarySpinner = findViewById(R.id.dictionarySpinner)
-        svjz = findViewById(R.id.svjzCheckBox)
-
-        val dictionarySpinnerListener = object : AdapterView.OnItemSelectedListener {
+        dictionarySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 if (position in 4..5 && mLocation == null) {
                     acquireLocation()
@@ -59,10 +49,7 @@ class PresmyslovnikActivity : LocationActivity() {
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
-        dictionarySpinner!!.onItemSelectedListener = dictionarySpinnerListener
-
-
-        val modeSpinnerListener = object : AdapterView.OnItemSelectedListener {
+        modeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 refreshSvjz()
                 inputEditText.inputType = if (position >= 6) InputType.TYPE_CLASS_NUMBER else InputType.TYPE_CLASS_TEXT
@@ -71,20 +58,18 @@ class PresmyslovnikActivity : LocationActivity() {
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
-        modeSpinner!!.onItemSelectedListener = modeSpinnerListener
-
         loadSavedState()
         refreshSvjz()
 
-        enDict = Dictionary(applicationContext, "en.canon", results!!)
-        czPJDict = Dictionary(applicationContext, "podst_jm_cz.canon", results!!)
-        czDict = Dictionary(applicationContext, "cs_CZ_openoffice.canon", results!!)
-        czBigDict = Dictionary(applicationContext, "cs.canon", results!!)
-        pragueMap = MapDictionary(applicationContext, "Prague.cbmap", results!!)
-        brnoMap = MapDictionary(applicationContext, "Brno.cbmap", results!!)
-        czechiaMap = MapDictionary(applicationContext, "Czechia.cbmap", results!!)
+        enDict = Dictionary(applicationContext, "en.canon", results)
+        czPJDict = Dictionary(applicationContext, "podst_jm_cz.canon", results)
+        czDict = Dictionary(applicationContext, "cs_CZ_openoffice.canon", results)
+        czBigDict = Dictionary(applicationContext, "cs.canon", results)
+        pragueMap = MapDictionary(applicationContext, "Prague.cbmap", results)
+        brnoMap = MapDictionary(applicationContext, "Brno.cbmap", results)
+        czechiaMap = MapDictionary(applicationContext, "Czechia.cbmap", results)
 
-        inputBox!!.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+        inputBox.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
             // If the event is a key-down event on the "enter" button
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                 searchDictionary()
@@ -93,60 +78,58 @@ class PresmyslovnikActivity : LocationActivity() {
             false
         })
 
-        inputBox!!.requestFocus()
+        inputBox.requestFocus()
 
-        goBtn!!.setOnClickListener { searchDictionary() }
+        goBtn.setOnClickListener { searchDictionary() }
     }
 
     private fun saveState() {
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val editor = sharedPreferences!!.edit()
-        editor.putInt("modeSpinner", modeSpinner!!.selectedItemPosition)
-        editor.putInt("dictionarySpinner", dictionarySpinner!!.selectedItemPosition)
+        val editor = sharedPreferences.edit()
+        editor.putInt("modeSpinner", modeSpinner.selectedItemPosition)
+        editor.putInt("dictionarySpinner", dictionarySpinner.selectedItemPosition)
         editor.apply()
     }
 
     private fun loadSavedState() {
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        modeSpinner!!.setSelection(sharedPreferences!!.getInt("modeSpinner", 0))
-        dictionarySpinner!!.setSelection(sharedPreferences!!.getInt("dictionarySpinner", 0))
+        modeSpinner.setSelection(sharedPreferences.getInt("modeSpinner", 0))
+        dictionarySpinner.setSelection(sharedPreferences.getInt("dictionarySpinner", 0))
     }
 
     private fun refreshSvjz() {
-        val dictionaryPosition = dictionarySpinner!!.selectedItemPosition
-        val modePosition = modeSpinner!!.selectedItemPosition
-        svjz!!.isEnabled = dictionaryPosition in 4..5 && modePosition in 2..3
+        val dictionaryPosition = dictionarySpinner.selectedItemPosition
+        val modePosition = modeSpinner.selectedItemPosition
+        svjz.isEnabled = dictionaryPosition in 4..5 && modePosition in 2..3
     }
 
     private fun searchDictionary() {
         val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputManager.hideSoftInputFromWindow(Objects.requireNonNull(currentFocus).windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
 
-        val modeId = modeSpinner!!.selectedItemPosition
+        val modeId = modeSpinner.selectedItemPosition
         var minLength = 0
-        if (minLengthBox!!.text.isNotEmpty()) {
-            minLength = Integer.parseInt(minLengthBox!!.text.toString())
+        if (minLengthBox.text.isNotEmpty()) {
+            minLength = Integer.parseInt(minLengthBox.text.toString())
         }
         var maxLength = 100
-        if (maxLengthBox!!.text.isNotEmpty()) {
-            maxLength = Integer.parseInt(maxLengthBox!!.text.toString())
+        if (maxLengthBox.text.isNotEmpty()) {
+            maxLength = Integer.parseInt(maxLengthBox.text.toString())
         }
-        results!!.text = "Result:\n"
+        results.text = "Result:\n"
 
-        val input = inputBox!!.text.toString().toLowerCase(Locale.ENGLISH)
+        val input = inputBox.text.toString().toLowerCase(Locale.ENGLISH)
 
         val findResults = fun(dict: Dictionary) { dict.findResults(input, modeId, minLength, maxLength)}
         val findResultsInMap = fun(map: MapDictionary) {
             if (mLocation == null) {
                 acquireLocation()
             }
-            map.setSvjz(svjz!!.isEnabled && svjz!!.isChecked)
+            map.setSvjz(svjz.isEnabled && svjz.isChecked)
             map.setLocation(mLocation!!)
             map.findResults(input, modeId, minLength, maxLength)
         }
 
         try {
-            when (dictionarySpinner!!.selectedItemPosition) {
+            when (dictionarySpinner.selectedItemPosition) {
                 0 -> findResults(enDict!!)
                 1 -> findResults(czPJDict!!)
                 2 -> findResults(czDict!!)
