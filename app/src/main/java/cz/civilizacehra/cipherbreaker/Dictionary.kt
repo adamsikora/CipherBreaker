a1a87018fc23b9f776ef5d8a5c65bef3bc94c71d
Adam Sikora <adam.sikora73@gmail.com>
2019-10-13 19:54:09 +0200

    Remove unused include

--- app/src/main/java/cz/civilizacehra/cipherbreaker/Dictionary.kt
+++ app/src/main/java/cz/civilizacehra/cipherbreaker/Dictionary.kt
@@ -6,7 +6,6 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.util.*
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 import kotlin.math.min
 
