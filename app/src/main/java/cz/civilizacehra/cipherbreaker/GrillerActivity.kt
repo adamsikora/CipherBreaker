8e692be7b9b5d0e51fe114330a40d650a5fe7b41
Adam Sikora <sikora@spaceknow.com>
2019-11-14 22:41:26 +0100

    Improve Grille input UX

--- app/src/main/java/cz/civilizacehra/cipherbreaker/GrillerActivity.kt
+++ app/src/main/java/cz/civilizacehra/cipherbreaker/GrillerActivity.kt
@@ -8,6 +8,7 @@ import android.text.Editable
 import android.text.InputType
 import android.text.TextWatcher
 import android.view.Gravity
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
@@ -33,7 +34,8 @@ class GrillerActivity : Activity() {
     private val grilleColors by lazy { arrayOf(
             ContextCompat.getColor(applicationContext, android.R.color.white),
             ContextCompat.getColor(applicationContext, R.color.colorPrimaryLight),
-            ContextCompat.getColor(applicationContext, R.color.lightGray)
+            ContextCompat.getColor(applicationContext, R.color.lightGray),
+            ContextCompat.getColor(applicationContext, R.color.gray)
     ) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -122,40 +124,73 @@ class GrillerActivity : Activity() {
             val tableRow = TableRow(this)
             tableRow.layoutParams
             for (j in 0 until size) {
-                // TODO special handling for middle cell in odd size
                 val cell = grille!![i][j]
                 cell.width = dimensions
                 cell.height = dimensions
                 // TODO make cell rectangular
-                cell.tag = 2
-                cell.setBackgroundColor(grilleColors[2])
-                cell.inputType = InputType.TYPE_CLASS_TEXT or EditorInfo.TYPE_TEXT_FLAG_CAP_CHARACTERS
-                cell.gravity = Gravity.CENTER_HORIZONTAL
-                cell.textSize = dimensions / 4.toFloat()
-                cell.setOnClickListener {
-                    setState(i, j)
-                }
-                cell.addTextChangedListener(object : TextWatcher {
-                    override fun afterTextChanged(s: Editable) {}
-                    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-                    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                        // TODO better behaviour for text input. Handle next/done, backspace.
-                        computeGrid()
-                        if (s.length == 1) {
-                            val nextJ = (j + 1) % size
-                            val nextI = if (nextJ == 0) i + 1 else i
-                            if (nextI == size) {
-                                val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                                inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-                            } else {
-                                grille!![nextI][nextJ].requestFocus()
-                            }
+                if (size % 2 == 1 && i == j && 2*i + 1 == size) {
+                    cell.tag = 3
+                    cell.setBackgroundColor(grilleColors[3])
+                    cell.isEnabled = false
+                } else {
+                    cell.tag = 2
+                    cell.setBackgroundColor(grilleColors[2])
+                    cell.inputType = InputType.TYPE_CLASS_TEXT or EditorInfo.TYPE_TEXT_FLAG_CAP_CHARACTERS
+                    cell.setSelectAllOnFocus(true)
+                    cell.gravity = Gravity.CENTER_HORIZONTAL
+                    cell.textSize = dimensions / 5.toFloat()
+
+                    fun goToNextCell() {
+                        val next = nextCell(i, j)
+                        if (next.first == size) {
+                            val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+                        } else {
+                            grille!![next.first][next.second].requestFocus()
                         }
                     }
-                })
-                cell.setOnLongClickListener {
-                    setState(i, j)
-                    return@setOnLongClickListener true
+                    fun goToPrevCell() {
+                        val prev = prevCell(i, j)
+                        if (prev.first == -1) {
+                            val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+                        } else {
+                            grille!![prev.first][prev.second].requestFocus()
+                        }
+                    }
+                    cell.setOnKeyListener{ _, keyCode, keyEvent ->
+                        when (keyCode) {
+                            KeyEvent.KEYCODE_ENTER -> {
+                                if (keyEvent.action == KeyEvent.ACTION_UP) {
+                                    goToNextCell()
+                                }
+                                true
+                            }
+                            KeyEvent.KEYCODE_DEL -> {
+                                if (keyEvent.action == KeyEvent.ACTION_DOWN && cell.text.isEmpty()) {
+                                    goToPrevCell()
+                                    true
+                                } else {
+                                    false
+                                }
+                            }
+                            else -> false
+                        }
+                    }
+                    cell.addTextChangedListener(object : TextWatcher {
+                        override fun afterTextChanged(s: Editable) {}
+                        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                            computeGrid()
+                            if (s.length == 1) {
+                                goToNextCell()
+                            }
+                        }
+                    })
+                    cell.setOnLongClickListener {
+                        setState(i, j)
+                        return@setOnLongClickListener true
+                    }
                 }
                 val cellLayout = LinearLayout(this)
                 val params = TableRow.LayoutParams(wrapContent, wrapContent)
@@ -177,6 +212,28 @@ class GrillerActivity : Activity() {
         grilleInputLayout.addView(envelopingLayout)
 
         computeGrid()
+    }
+
+    private fun isCenterCell(i: Int, j: Int): Boolean {
+        return size % 2 == 1 && i == j && 2*i + 1 == size
+    }
+
+    private fun nextCell(i: Int, j: Int): Pair<Int, Int> {
+        var nextJ = (j + 1) % size
+        val nextI = if (nextJ == 0) i + 1 else i
+        if (isCenterCell(nextI, nextJ)) {
+            nextJ += 1
+        }
+        return Pair(nextI, nextJ)
+    }
+
+    private fun prevCell(i: Int, j: Int): Pair<Int, Int> {
+        var prevJ = (j - 1 + size) % size
+        val prevI = if (j == 0) i - 1 else i
+        if (isCenterCell(prevI, prevJ)) {
+            prevJ -= 1
+        }
+        return Pair(prevI, prevJ)
     }
 
     private fun setState(i: Int, j: Int) {
@@ -229,7 +286,7 @@ class GrillerActivity : Activity() {
                 }
                 letters += letter
                 val tag = cell.tag as Int
-                if (tag != 2) {
+                if (tag < 2) {
                     isEmpty = false
                 }
                 holes += tag.toString()
@@ -265,7 +322,7 @@ class GrillerActivity : Activity() {
                     cell.setText(inputLetter)
                 }
                 val tag = inputHoles[i*size + j].toString().toInt()
-                if (tag < 0 || tag > 2) {
+                if (tag < 0 || tag > 3) {
                     applicationContext.toastIt("Invalid cell state $tag")
                     return
                 }
