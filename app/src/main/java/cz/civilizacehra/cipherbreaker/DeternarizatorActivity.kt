b7ec859373e8eec101d5d35b34aff683861fb193
Adam Sikora <adam.sikora73@gmail.com>
2019-11-15 09:52:07 +0100

    Improve Deternarizator settings UX

--- app/src/main/java/cz/civilizacehra/cipherbreaker/DeternarizatorActivity.kt
+++ app/src/main/java/cz/civilizacehra/cipherbreaker/DeternarizatorActivity.kt
@@ -1,16 +1,20 @@
 package cz.civilizacehra.cipherbreaker
 
 import android.os.Bundle
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.Switch
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
 class DeternarizatorActivity : DebaseatorActivity() {
-    internal val mode by lazy { findViewById<Switch>(R.id.modeSwitch) }
-    internal val direction by lazy { findViewById<Switch>(R.id.directionSwitch) }
-    internal val ch by lazy { findViewById<CheckBox>(R.id.chCheckBox) }
+    internal val mode by lazy { findViewById<RadioGroup>(R.id.modeRadioGroup) }
+    internal val direction by lazy { findViewById<RadioGroup>(R.id.directionRadioGroup) }
+    internal val directionRight by lazy { findViewById<RadioButton>(R.id.rbtnRight) }
+    internal val directionLeft by lazy { findViewById<RadioButton>(R.id.rbtnLeft) }
+    internal val ch by lazy { findViewById<Switch>(R.id.chSwitch) }
+
+    private val settingsLayout by lazy { findViewById<RelativeLayout>(R.id.settingsLayout) }
+    private val closeIcon by lazy { findViewById<RelativeLayout>(R.id.closeIconLayout) }
+    private val settingsIcon by lazy { findViewById<RelativeLayout>(R.id.settingsIconLayout) }
+
     internal val images by lazy { arrayOf<ImageView>(
             findViewById(R.id.interpretation1),
             findViewById(R.id.interpretation2),
@@ -47,26 +51,41 @@ class DeternarizatorActivity : DebaseatorActivity() {
         commonInit(3, 3, R.layout.ternaryrow)
 
         direction.setOnCheckedChangeListener { _, _ -> updateRows() }
-        mode.setOnCheckedChangeListener { _, isChecked ->
-            direction.isEnabled = !isChecked
-            if (isChecked) {
-                images[0].setImageResource(R.drawable.ternaryorder6)
-                images[1].setImageResource(R.drawable.ternaryorder5)
-                images[2].setImageResource(R.drawable.ternaryorder4)
-                images[3].setImageResource(R.drawable.ternaryorder3)
-                images[4].setImageResource(R.drawable.ternaryorder2)
-                images[5].setImageResource(R.drawable.ternaryorder1)
-            } else {
-                images[0].setImageResource(R.drawable.ternary1)
-                images[1].setImageResource(R.drawable.ternary2)
-                images[2].setImageResource(R.drawable.ternary3)
-                images[3].setImageResource(R.drawable.ternary4)
-                images[4].setImageResource(R.drawable.ternary5)
-                images[5].setImageResource(R.drawable.ternary6)
+        mode.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rbtnOrder -> {
+                    directionRight.isEnabled = false
+                    directionLeft.isEnabled = false
+                    images[0].setImageResource(R.drawable.ternaryorder6)
+                    images[1].setImageResource(R.drawable.ternaryorder5)
+                    images[2].setImageResource(R.drawable.ternaryorder4)
+                    images[3].setImageResource(R.drawable.ternaryorder3)
+                    images[4].setImageResource(R.drawable.ternaryorder2)
+                    images[5].setImageResource(R.drawable.ternaryorder1)
+                }
+                R.id.rbtnValues -> {
+                    directionRight.isEnabled = true
+                    directionLeft.isEnabled = true
+                    images[0].setImageResource(R.drawable.ternary1)
+                    images[1].setImageResource(R.drawable.ternary2)
+                    images[2].setImageResource(R.drawable.ternary3)
+                    images[3].setImageResource(R.drawable.ternary4)
+                    images[4].setImageResource(R.drawable.ternary5)
+                    images[5].setImageResource(R.drawable.ternary6)
+                }
             }
             updateRows()
         }
         ch.setOnCheckedChangeListener { _, _ -> updateRows() }
+
+        closeIcon.setOnClickListener { settingsLayout.visibility = View.GONE }
+        settingsIcon.setOnClickListener {
+            if (settingsLayout.visibility == View.GONE) {
+                settingsLayout.visibility = View.VISIBLE
+            } else {
+                settingsLayout.visibility = View.GONE
+            }
+        }
     }
 
     override fun onRowClick(layout: RelativeLayout) {
@@ -75,13 +94,13 @@ class DeternarizatorActivity : DebaseatorActivity() {
             val value = layout.findViewById<ImageView>(bits!![k]).tag as Int
             values[k] = value
         }
-        val iterate = if (direction.isChecked) intArrayOf(2, 1, 0) else intArrayOf(0, 1, 2)
+        val iterate = if (direction.checkedRadioButtonId == R.id.rbtnRight) intArrayOf(2, 1, 0) else intArrayOf(0, 1, 2)
 
         val offset = if (alphabetStart!!.checkedRadioButtonId == R.id.rbtn0) 1 else 0
 
         for (i in 0..5) {
             var value = 0
-            if (mode.isChecked) {
+            if (mode.checkedRadioButtonId == R.id.rbtnOrder) {
                 for (j in mapping[5 - i]) {
                     value *= mBase
                     value += values[j]
