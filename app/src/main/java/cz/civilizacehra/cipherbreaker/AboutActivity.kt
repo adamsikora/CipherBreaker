7c80e0e8fb311fabf59b06c8793356fa57d1768b
Adam Sikora <adam.sikora73@gmail.com>
2025-01-02 17:52:51 +0100

    Rework about page
    The library used before had some crashes.
    Fully reimplemented to avoid usage of library.

--- app/src/main/java/cz/civilizacehra/cipherbreaker/AboutActivity.kt
+++ app/src/main/java/cz/civilizacehra/cipherbreaker/AboutActivity.kt
@@ -2,33 +2,17 @@ package cz.civilizacehra.cipherbreaker
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.Gravity
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.TextView
 
-import mehdi.sakout.aboutpage.AboutPage
-import mehdi.sakout.aboutpage.Element
 
 class AboutActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-
-        val copyRightsElement = Element()
-        copyRightsElement.title = "© Adam Sikora 2023-2025"
-        copyRightsElement.gravity = Gravity.CENTER
-
-        val aboutPage = AboutPage(this)
-                .isRTL(false)
-                .setDescription("This app was created to help with Puzzle Hunt competitions. " +
-                                "Note that it contains some powerful tools and usage of some " +
-                                "might be against the rules of some particular Puzzle Hunts. " +
-                                "Suggestions or contributions on github are welcome. ")
-                .addItem(Element().setTitle("Version 1.4.0"))
-                .addEmail("adam.sikora73@gmail.com")
-                .addGitHub("adamsikora/CipherBreaker")
-                .addItem(copyRightsElement)
-                .create()
-
-        setContentView(aboutPage)
+        val githubLink = findViewById<View>(R.id.githubView) as TextView
+        githubLink.movementMethod = LinkMovementMethod.getInstance()
     }
 }
